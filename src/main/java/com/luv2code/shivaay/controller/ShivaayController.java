6f017c9ddbfe1b1ebe4ad82ba9a99ffa5924e989
mickey005 <bansalarchit078@gmail.com>
2021-01-27 15:29:56 +0530

    home page

--- src/main/java/com/luv2code/shivaay/controller/ShivaayController.java
+++ src/main/java/com/luv2code/shivaay/controller/ShivaayController.java
@@ -3,7 +3,10 @@ package com.luv2code.shivaay.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.shivaay.entity.Mail;
 
 @Controller
 @RequestMapping("/")
@@ -11,8 +14,9 @@ public class ShivaayController {
 	
 	
 	@RequestMapping("/home")
-	public String getHomePage() {
+	public String getHomePage(Model theModel) {
 		
+		theModel.addAttribute("mail",new Mail());
 		return "pages/home";
 	}
 	
@@ -22,8 +26,19 @@ public class ShivaayController {
 		
 
 		
-		return "/pages/themeImpementation";
+		return "pages/themeImpementation";
 		
+	}
+	
+	@RequestMapping("/mail")
+	public String getHomePageMail(@ModelAttribute Mail theEmail) {
+		
+		System.out.println("anand >> email >>"+theEmail.getEmail());
+		
+		System.out.println(theEmail.getName());
+		
+		//theModel.addAttribute("mail",new Mail());
+		return "pages/home";
 	}
 
 }
